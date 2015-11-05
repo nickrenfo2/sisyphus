ee8b26869e1e15de3e9a6e50477db955c3f30977
Nick Renford <nickrenfo@gmail.com>
2015-11-05 11:01:55 -0600

    fix registration. sisbots are now added upon registration, if they do not already exist

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,6 +3,7 @@ var path = require('path');
 var router = express.Router();
 var User = require('../models/User');
 var Sisbot = require('../models/Sisbot');
+var passport = require('passport');
 
 
 router.get('/', function (req,res) {
@@ -31,41 +32,57 @@ router.post('/acct/login', function (req,res) {
 
 router.post('/acct/register', function (req,res) {
     console.log("registering user:",req.body);
+    //Create a new user
     User.create({
         email:req.body.email,
         password:req.body.password,
-        sisSerials:['test']
+        sisSerials:['test'] //initialize serials
     }, function (err,usr) {
         if(err){
+            //something broke, account not created
             console.log('error:',err);
             return res.send({accountStatus: "fail", serialStatus: "fail"})
         }
         console.log(usr);
-        if (checkSerial(req.body.serial,req.body.sid)) {
-            usr.sisSerials = [req.body.serial];
-            return res.send({accountStatus: "success", serialStatus: "success"})
-        } else {
-
-            return res.send({accountStatus: "success", serialStatus: "fail"})
-        }
+        var serial = req.body.serial;
+        var sid = req.body.sid;
+        //Check to see if sisbot exists
+        Sisbot.findOne({serial:serial}, function (err,sisbot) {
+            //If sisbot doesn't exist, create it
+            if (!sisbot){
+                Sisbot.create({
+                    serial:serial,
+                    sid:sid,
+                    state:{}
+                }, function (err,sis) {
+                    if (err) {
+                        //if something broke while creating the sisbot
+                        console.log('error creating sisbot:', err);
+                        return res.send({accountStatus: "success", serialStatus: "fail"})
+                    }
+                    else {
+                        //sisbot created successfully, set the account to be able to control the new sisbot
+                        usr.sisSerials = [sis.serial];
+                        usr.save();
+                        return res.send({accountStatus: "success", serialStatus: "success"})
+                    }
+                });
+            } else { //if sisbot already exists
+                if (sid == sisbot.sid) { //make sure user has correct sid
+                    console.log('serial and sid match');
+                    usr.sisSerials = [serial];
+                    usr.save(function (err,usr) { //allow user to control sisbot
+                        if (err) console.log('error updating user:',err); //something broke
+                    });
+                    return res.send({accountStatus: "success", serialStatus: "success"})
+                } else { //user does not have correct sid for this serial, thus do not allow them to control it
+                    console.log('serial and sid do not match');
+                    return res.send({accountStatus: "success", serialStatus: "fail"})
+                }
+            }
+        });
     })
 });
-
-//Check that a given sid matches that of a sisbot with the given serial number
-function checkSerial(serial,sid){
-    //check sisbots for serial, then check sid
-    Sisbot.findOne({serial:serial}, function (err,sisbot) {
-        if (!sisbot || err){
-            console.log("no sisbot found or error happened");
-        } else {
-            console.log("found sisbot:", sisbot);
-            if(sisbot.sid == sid)
-                return true;
-            else return false;
-        }
-    });
-
-}
 
 
 
